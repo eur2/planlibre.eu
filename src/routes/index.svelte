82d98ead190a7f9cf0c2bbb91c8e9b63423785fd
(eur) <28712725+eur2@users.noreply.github.com>
2020-05-01 19:14:17 +0200

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,31 +26,46 @@ article{border-top:1px solid black;}
 		<article>
 			<header>
 			<div class="flex">
+			<h2>{post.acf.num}</h2>
+			<h2>{post.acf.title}</h2>
 			<h2>{post.acf.date}</h2>
-			<a href='{post.acf.pdf}' target="_blank">PDF</a>
+			<h2><a href='{post.acf.pdf}' target="_blank">PDF</a></h2>
 			</div>
-			<img src={post.acf.couverture.sizes.thumbnail} width="200" alt="dg"/>
+			<img src={post.acf.cover.sizes.thumbnail} width="200" alt="dg"/>
 			</header>
 			<div>
 			{#if post.acf.image}
-			<img src={post.acf.image.sizes.thumbnail} alt="dg"/>
+			<img src={post.acf.image.sizes.thumbnail} width="600" alt="dg"/>
 			{/if}
-			{#if post.acf.article1.pdf === false}
+			{#if post.acf.article1.title === ''}
 			<div></div>
 			{:else}
 			<div>
-				<h2>{post.acf.article1.titre}</h2>
+				<h3>{post.acf.article1.author}</h3>
+				<h3>{post.acf.article1.title}</h3>
 				<a href='{post.acf.article1.pdf}' target="_blank">PDF</a>
 			</div>
 			{/if}
-			{#if post.acf.article2.pdf === false}
+			{#if post.acf.article2.pdf === ''}
 			<div></div>
 			{:else}
 			<div>
-				<h2>{post.acf.article2.titre}</h2>
+				<h3>{post.acf.article2.author}</h3>
+				<h3>{post.acf.article2.title}</h3>
 				<a href='{post.acf.article2.pdf}' target="_blank">PDF</a>
 			</div>
 			{/if}
+
+{#if post.acf.article3.pdf === ''}
+			<div></div>
+			{:else}
+			<div>
+				<h3>{post.acf.article3.author}</h3>
+				<h3>{post.acf.article3.title}</h3>
+				<a href='{post.acf.article3.pdf}' target="_blank">PDF</a>
+			</div>
+			{/if}
+
 
 		</div>
 
